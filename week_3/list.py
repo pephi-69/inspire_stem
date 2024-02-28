39b241bcc63d3e4320cc1fb6df8a74e72454657f
pephi-69 <pephraim857@gmail.com>
2024-02-28 08:25:53 +0300

    list,loops

---
+++ week_3/list.py
@@ -0,0 +1,41 @@
+#items = ["pen",'book','ruler',"set"]
+#print(items)
+#append("laptop")
+#insert("laptop")
+#pop()
+#remove("book")
+#index ()
+#count
+#sort
+#reverse
+#print('\n')
+#items.append("laptop")
+
+
+#print('\n')
+#items.pop()
+
+
+#print('\n')
+#items.reverse()
+#print(items)
+
+#list of 5 movies--pop two ,sort,reverse,count
+  
+movies ={"underdogs","cobweb","players"," the hustle"}
+
+#movies.pop()
+#movies.pop()
+#print(movies)
+#print('\n')
+
+#sorted_movies = sorted(movies)
+#print(sorted_movies)
+#print('\n')
+
+#movies.reverse()
+#print(movies)
+#rint('\n')
+
+count = len(movies)
+print(count)
