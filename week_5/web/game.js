b08825ca6ae6131f8da7cd376af2e501e189ef3f
pephi-69 <pephraim857@gmail.com>
2024-03-20 10:18:04 +0300

    list,loops

--- week_5/web/game.js
+++ week_5/web/game.js
@@ -1,4 +1,28 @@
 alert("welcome winner sikee")
 
 let Message="welcome "
-document.write(Message)    
+document.write(Message)   
+
+const randomNumber = Math.floor(Math.random()*100)+1
+let  attempt = 0;
+
+function checkguess (){
+    const guess = parseInt(document.getElementById("guessField").value)
+    attempt ++;
+    if(isNaN(guess) || guess <1 || guess >100 ){
+        setMessage("please enter a valid number btn 1 to 100")
+        return
+    }
+    if (guess === randomNumber){
+        setMessage("you win")
+        document.getElementById("guessField").disabled = true
+    }else if(guess < randomNumber){
+        setMessage("go higher")
+    }else {
+        setMessage("go lower")
+    }
+function setMessage(Message){
+    document.getElementById("Message").textContent = Message
+}
+
+}
